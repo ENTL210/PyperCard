37f6a409592c574df4bfac14b0b85011709e8ab5
Lam_Edward <lame972@hsd.k12.or.us>
2023-11-29 10:00:41 -0800

    Working on the input page & adding functions that fetch the api to the local database for future usages

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ weather_app = App()
 # Patch the Requests Libarary so it works with Pyscript
 pyodide_http.patch_all()
 
-# In the "get_name" card, when you "click" on the "submit" button...
-@weather_app.transition("get_name", "click", "submit")
+# In the "input" card, when you "click" on the "submit" button...
+@weather_app.transition("input-card", "click", "submit-btn")
 def hello(app, card):
     """
     Store the value in the card's input box, with the id "name", into the app's
@@ -28,30 +28,39 @@ def hello(app, card):
 
     Then transition to the "say_hello" card.
     """
-    app.datastore["name"] = card.get_by_id("name").value
+    app.datastore["city-input"] = card.get_by_id("city-input").value
 
     # Required parameters to fetch the weather api...
     parameters = {
     'key': '64c4ff4519da47c6bf6224642231911',
-    'q': f'{app.datastore["name"]}',
+    'q': f'{app.datastore["city-input"]}',
     'aqi': 'no'
     }
     
     # Fetch the api...
     api_call = requests.get("http://api.weatherapi.com/v1/current.json", params=parameters)
 
-    app.datastore["data"] = api_call.text
+    # parse the entire api as a json to the local database...
+    app.datastore["data"] = api_call.json()
 
-    return "say_hello"
+    # Loop through the 'location' & 'current' section of the api, and add data to the database...
+    for items in app.datastore["data"]["location"]:
+        app.datastore[items] = app.datastore["data"]["location"][items]
+
+    for items in app.datastore["data"]["current"]:
+        app.datastore[items] = app.datastore["data"]["current"][items]
 
 
-# In the "say_hello" card, when you "click" on the "again" button...
-@weather_app.transition("say_hello", "click", "again")
+    return "result-card"
+
+
+# In the "result_card" card, when you "click" on the "again" button...
+@weather_app.transition("result-card", "click", "again")
 def again(app, card):
     """
     Don't do anything except transition to the "get_name" card.
     """
-    return "get_name"
+    return "input-card"
 
 
 # Start the hello_app
